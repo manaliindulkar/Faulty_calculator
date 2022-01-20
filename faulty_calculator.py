41ae0142c923fa8dca64219966ae29ed1b3c3dfd
manaliindulkar <98088663+manaliindulkar@users.noreply.github.com>
2022-01-20 02:12:05 -0800

    Add files via upload

---
+++ faulty_calculator.py
@@ -0,0 +1,25 @@
+print("enter 1st no:")
+n1=int(input())
+print("enter 2nd no:")
+n2=int(input())
+print("enter operation")
+n3=input()
+if n1==56 and n2==9 and n3=="+":
+    print("77")
+elif n3=="+":
+    print("sum is",n1+n2)
+elif n3=="-":
+    print("subs ",n1-n2)
+elif n1==45 and n2==3 and n3=="*" :
+    print("555")
+elif n3=="*":
+    print("multiplication is ", n1 * n2)
+elif n1 == 56 and n2 == 6 and n3 == "/":
+    print("4")
+elif n3=="/":
+    print("divisionis ",n1/n2)
+else:
+    print("invalid input")
+
+
+
